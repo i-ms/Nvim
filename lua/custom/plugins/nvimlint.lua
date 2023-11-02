8098255270fc864a0d124c5abe482a2ca50437ca
Mayank Singh <mayanksingh0068@gmail.com>
2023-11-03 01:31:46 +0530

    config : nvimlint configuration update
    This configurations enables to run linter automatically on events like BufEnter,Exit and Leaving the Insert mode

--- lua/custom/plugins/nvimlint.lua
+++ lua/custom/plugins/nvimlint.lua
@@ -3,11 +3,17 @@ return {
     event = "VeryLazy",
     config = function()
       require('lint').linters_by_ft = {
-        javascript = {"eslint"},
-        typescript = {"eslint"},
-      }
-
-      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      python = { "pylint" },
+    }
+      local lint_augroup = vim.api.nvim_create_autocmd("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        pattern = { "*.ts", "*.js"},
+        group = lint_augroup,
         callback = function()
           require("lint").try_lint()
         end,
